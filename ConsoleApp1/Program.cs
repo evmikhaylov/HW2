17061aae981b80e87c6642ab6b666c393060401b
Михайлов Евгений <jackmihailov@outlook.com>
2021-10-05 16:34:08 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace HELLO_WORLD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello");
+            Console.Write("World");
+            Console.Write("!!!");
+
+            Console.WriteLine("\nHello ");
+            Console.WriteLine("World ");
+            Console.WriteLine("!!! ");
+            Console.WriteLine("Нажать ENTER  для выхода");
+            while (Console.ReadKey().Key != ConsoleKey.Enter) ;
+        }
+    }
+}
